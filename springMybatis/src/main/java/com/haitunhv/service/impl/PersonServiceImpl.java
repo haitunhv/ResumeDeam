26725f9ecc6e05f3102bbd5953e9da8fdc95cc28
gaoshishun <gaoshishun@yundait.com>
2021-03-07 21:34:05 +0800

    注解切面
    Signed-off-by: haitunhv <haitunhv@163.com>

--- springMybatis/src/main/java/com/haitunhv/service/impl/PersonServiceImpl.java
+++ springMybatis/src/main/java/com/haitunhv/service/impl/PersonServiceImpl.java
@@ -2,12 +2,14 @@ package com.haitunhv.service.impl;
 
 import com.haitunhv.service.PersonService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author: haitunhv
  * @Date: 2021/3/31 20:34
  */
 @Service
+@Transactional
 public class PersonServiceImpl implements PersonService {
     @Override
     public boolean save(String name, Integer level) {
