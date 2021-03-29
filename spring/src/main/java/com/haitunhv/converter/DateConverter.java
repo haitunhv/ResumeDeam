bfa505d3648f1de410c65485ab5ff2a8d8cab9ac
haitunhv <haitunhv@163.com>
2021-03-29 22:01:33 +0800

    转换器
    Signed-off-by: haitunhv <haitunhv@163.com>

---
+++ spring/src/main/java/com/haitunhv/converter/DateConverter.java
@@ -0,0 +1,30 @@
+package com.haitunhv.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Author: gss
+ * @Date: 2021/3/29 21:31
+ */
+public class DateConverter implements Converter<String, Date> {
+    private String frm;
+
+    public void setFrm(String frm) {
+        this.frm = frm;
+    }
+
+    @Override
+    public Date convert(String s) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat(frm);
+            return format.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
