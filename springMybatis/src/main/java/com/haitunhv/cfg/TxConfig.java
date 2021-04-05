ba3a9e5ccad5b472d1d2387c8ada7f1fae9768c4
haitunhv <haitunhv@163.com>
2021-04-05 21:01:10 +0800

    注解整合事务
    Signed-off-by: haitunhv <haitunhv@163.com>

---
+++ springMybatis/src/main/java/com/haitunhv/cfg/TxConfig.java
@@ -0,0 +1,23 @@
+package com.haitunhv.cfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author: gss
+ * @Date: 2021/4/5 20:53
+ */
+@Configuration
+@EnableTransactionManagement
+public class TxConfig {
+    @Bean
+    public DataSourceTransactionManager mgr(DataSource dataSource){
+        DataSourceTransactionManager mgr = new DataSourceTransactionManager();
+        mgr.setDataSource(dataSource);
+        return mgr;
+    }
+}
