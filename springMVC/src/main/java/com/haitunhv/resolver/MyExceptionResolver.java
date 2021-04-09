5bf5a3a108505dcd27dbac3bd43a505a5b0243ea
haitunhv <haitunhv@163.com>
2021-04-09 22:07:40 +0800

    异常处理
    Signed-off-by: haitunhv <haitunhv@163.com>

---
+++ springMVC/src/main/java/com/haitunhv/resolver/MyExceptionResolver.java
@@ -0,0 +1,26 @@
+package com.haitunhv.resolver;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: gss
+ * @Date: 2021/4/9 21:52
+ */
+@Component
+public class MyExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest,
+                                         HttpServletResponse httpServletResponse,
+                                         Object o,
+                                         Exception e) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("ex",e);
+        mv.setViewName("/WEB-INF/page/error/default.jsp");
+        return mv;
+    }
+}
---
+++ springMVC/src/main/webapp/WEB-INF/page/error/default.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: OMEN
+  Date: 2021/4/9
+  Time: 21:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+This is default.jsp异常信息:${ex.message}
+</body>
+</html>
