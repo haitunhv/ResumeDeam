24881d5e52ecc0339d41972863b80cc9b9e2238d
haitunhv <haitunhv@163.com>
2021-04-22 22:05:48 +0800

    log4j

---
+++ log4j/src/main/java/com/haitunhv/TestLog4j.java
@@ -0,0 +1,22 @@
+package com.haitunhv;
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+
+/**
+ * @Author: gss
+ * @Date: 2021/4/20 22:09
+ */
+public class TestLog4j {
+    public static void main(String[] args) {
+        Logger logger = new LogManager().getLogger(TestLog4j.class);
+
+        logger.fatal("致命_FATAL");
+        logger.error("错误_ERROR");
+        logger.warn("警告_WARN");
+        logger.info("信息_INFO");
+        logger.debug("调试_DEBUG");
+        logger.trace("痕迹_TRACE");
+    }
+}
