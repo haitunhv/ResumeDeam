0fa49183827d1d0344c9493543712c94e594d2c4
haitunhv <haitunhv@163.com>
2021-05-08 21:43:09 +0800

    初始化驾考分支

--- mybatis/src/main/java/com/haitunhv/Application.java
+++
@@ -1,8 +0,0 @@
-package com.haitunhv;
-
-/**
- * Author: haitunhv
- * @Date: 2021/4/18 22:42
- */
-public class Application {
-}
--- thymeleaf/src/main/java/com/haitunhv/Application.java
+++
@@ -1,8 +0,0 @@
-package com.haitunhv;
-
-/**
- * Author: haitunhv
- * @Date: 2021/4/17 22:21
- */
-public class Application {
-}
